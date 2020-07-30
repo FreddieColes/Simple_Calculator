80ee0ffcf75762871c8819d4f7296fbb977ba911
Freddie Coles <freddie.coles1@hotmail.co.uk>
2020-07-30 10:11:39 +0100

    Simple_Calculator visual studio files

---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Fred's calculator! \nType your first number, then your second number, then an operation.");
+            Start:
+            try
+            {                
+                InputConverter inputConverter = new InputConverter();
+
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                //In real world we would want to log this message
+                Console.WriteLine(ex.Message);
+                goto Start;
+            }
+            goto Start;
+        }
+    }
+}
